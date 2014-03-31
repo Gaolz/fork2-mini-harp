fec9542a2d18f34e86a09aa36df52e6858cbda3a
glz <glz@mail.com>
2014-03-31 13:12:37 +0800

    current-time

--- index.js
+++ index.js
@@ -2,18 +2,37 @@
   var mini_harp;
   var connect = require('connect');
   var app = connect();
+  var body = (new Date()).toISOString();
 
   module.exports = mini_harp = function(port) {
     if(port === undefined) {
       return (function() {
         console.log("Starting http server on http://localhost:4000");
-	app.listen(4000);
+        app.use(function(req,res) {
+          if (req.url === '/current-time') {
+	    res.writeHead(200, { 'Content-Type': 'text/plain'});
+            res.end(body + "\n");
+          }
+          else {
+	    res.writeHead(404, { 'Content-Type': 'text/plain'});
+            res.end("Can't Get " + req.url + "\n");
+          }
+	}).listen(4000);
       }).call(this);
     }
     else {
       return (function() {
         console.log("Starting http server on http://localhost:" + port);
-	app.listen(port);
+	app.use(function(req,res) {
+	  if (req.url === '/current-time') {
+	    res.writeHead(200, { 'Content-Type': 'text/plain'});
+	    res.end(body + "\n");
+	  }
+	  else {
+	    res.writeHead(404, { 'Content-Type': 'text/plain'});
+	    res.end("Can't Get " + req.url + "\n");
+	  }
+	}).listen(port);
       }).call(this);
     }
   }
