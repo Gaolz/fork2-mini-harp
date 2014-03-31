a7ff873db55c970b50c4a6b60c1d39b606eceea8
glz <glz@mail.com>
2014-03-31 11:44:56 +0800

    create-command

---
+++ bin/mini-harp.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+var command = require('../lib/command');
+console.log("hello, world");
+command();
+console.log("hello, man");
--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-var createMiniHarp = require("mini-harp")
-  , app = createMiniHarp();
-console.log("Starting mini-harp on http://localhost:4000");
-app.listen(4000);
+(function() {
+  var mini_harp;
+  var connect = require('connect');
+  var app = connect();
+
+  module.exports = mini_harp = function(port) {
+    if(port === undefined) {
+      return (function() {
+        console.log("Starting http server on http://localhost:4000");
+	app.listen(4000);
+      }).call(this);
+    }
+    else {
+      return (function() {
+        console.log("Starting http server on http://localhost:" + port);
+	app.listen(port);
+      }).call(this);
+    }
+  }
+}).call(this);
