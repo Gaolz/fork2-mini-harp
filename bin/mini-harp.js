3f126e868809f11f7cf954a8ce49aed4e784ad3e
glz <glz@mail.com>
2014-03-31 15:50:54 +0800

    modified bin/mini-harp.js

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
-(function() {
-    var parseArgs = require('minimist');
-    var createMiniHarp = require('../index.js');
-    var argv = parseArgs(process.argv.slice(2));
-    var port = 'port' in argv ? parseInt(argv.port) : 4000;
-    var root = argv['_'].length > 0 ? argv['_'][0] : process.cwd();
 
-    createMiniHarp(root).listen(port);
-    console.log('Starting mini-harp on http://localhost:' + port + "\n"); 
-})();
+var miniHarp = require("mini-harp");
+var parseArgs = require('minimist');
+var argv = parseArgs(process.argv.slice(2));
+var port = 'port' in argv ? parseInt(argv.port) : 4000;
+var root = argv['_'].length > 0 ? argv['_'][0] : process.cwd();
+
+miniHarp(root).listen(port);
+console.log('Starting mini-harp on http://localhost:' + port + "\n"); 
